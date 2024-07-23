cfb4488ee47797b3ee0f3a17dbf524d6b96615b8
Jiyeon Park <wldusys34@gmail.com>
2024-07-23 20:48:58 +0900

    Test: 음성인식 및 텍스 변환 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "ko-KR" });
+  const stopListening = () => SpeechRecognition.stopListening();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>브라우저가 음성 인식을 지원하지 않습니다.</span>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="prose p-4">
+      <h2>음성 인식 & 텍스트로 변환 테스트!!</h2>
+      <div className="flex gap-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={startListening}
+          disabled={listening}
+        >
+          말하기
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={stopListening}
+          disabled={!listening}
+        >
+          일시정지
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={resetTranscript}
+        >
+          Reset
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div>
+        <h3>텍스트 변환</h3>
+        <div className="border w-96 h-40 rounded">
+          <p>{transcript}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
