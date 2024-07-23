d95cc035bce14c2724265c4353415620f697a803
Jiyeon Park <wldusys34@gmail.com>
2024-07-23 20:48:17 +0900

    Chore: react-speech-recognition 설치 및 babel 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from "vite-plugin-babel";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: ["@babel/plugin-transform-runtime"],
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ["@babel/runtime/regenerator"],
+    },
+  },
+});
