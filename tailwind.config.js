d5227a732d3f2fd6ebcc6fa244fbf6f5677e7abd
Jiyeon Park <wldusys34@gmail.com>
2024-07-25 03:45:38 +0900

    Design: html 크기 및 배경색 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ import typography from "@tailwindcss/typography";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1025px",
+      xl: "1200px",
+    },
     extend: {},
   },
   plugins: [typography],
