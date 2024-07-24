723bfb414f726eb80620025defc26cc9a9bed390
Jiyeon Park <wldusys34@gmail.com>
2024-07-25 03:19:37 +0900

    Test: lottie 파일 적용 테스트

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFoundPage from "./pages/NotFoundPage";
 import SpeechRec from "./test/SpeechRec";
+import LottieTest from "./test/LottieTest";
 
 const Router = (): JSX.Element => {
   return (
@@ -9,6 +10,7 @@ const Router = (): JSX.Element => {
       <Route path="*" element={<NotFoundPage />} />
       <Route path="/" element={<Index />} />
       <Route path="/speech" element={<SpeechRec />} />
+      <Route path="/lottie" element={<LottieTest />} />
     </Routes>
   );
 };
---
+++ src/test/LottieTest.tsx
@@ -0,0 +1,8 @@
+import Lottie from "lottie-react";
+import LottieData from "../assets/lottie/listen.json";
+
+const LottieTest = () => {
+  return <Lottie className="w-[120px] h-[120px]" animationData={LottieData} />;
+};
+
+export default LottieTest;
