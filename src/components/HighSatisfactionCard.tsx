d38c61656437615e2e84fd189557798a502db1ec
Jiyeon Park <wldusys34@gmail.com>
2024-07-25 19:19:15 +0900

    Feat: 가격에 쉼표 구현

--- src/components/HighSatisfactionCard.tsx
+++ src/components/HighSatisfactionCard.tsx
@@ -1,12 +1,16 @@
 import { ProductCard } from "../type";
 
 const HighSatisfactionCard = ({ data }: { data: ProductCard }) => {
+  const formattedPrice = data.price.toLocaleString("ko-KR");
+
   return (
     <div>
       <img className="rounded-lg" src={data.imgSrc} alt="상품 사진" />
       <div>
         <div className="pt-2 pb-1 text-md font-medium">{data.productName}</div>
-        <div className="text-sm text-gray-500 font-normal">{data.price}원</div>
+        <div className="text-sm text-gray-500 font-normal">
+          {formattedPrice}원
+        </div>
       </div>
     </div>
   );
