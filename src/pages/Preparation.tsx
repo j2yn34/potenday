c83c5a782c973c7877382086e6f75fc3fd302537
Jiyeon Park <wldusys34@gmail.com>
2024-07-28 16:55:55 +0900

    Feat: 서비스 준비중 화면에 홈으로 가기 버튼 구현

--- src/pages/Preparation.tsx
+++ src/pages/Preparation.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import prepare from "../assets/images/prepare.svg";
 
 const Preparation = () => {
@@ -8,6 +9,12 @@ const Preparation = () => {
         <span className="text-xl font-semibold">서비스 준비중이에요.</span>
         <span className="text-gray-500 text-sm">조금만 기다려주세요.</span>
       </div>
+      <Link
+        to="/"
+        className="min-w-fit bg-black text-white px-7 py-5 rounded-lg mt-14"
+      >
+        홈으로 가기
+      </Link>
     </div>
   );
 };
