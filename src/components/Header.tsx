efaea31dd44918f100c3e55a47e19105d6eb4acd
Jiyeon Park <wldusys34@gmail.com>
2024-07-28 15:32:00 +0900

    Fix: 오타 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
           <Link to="/preparing">
             <img src={heartIcon} alt="관심목록" />
           </Link>
-          <Link to="/preparation">
+          <Link to="/preparing">
             <img src={userIcon} alt="내 정보" />
           </Link>
         </div>
