8109cb28d705f15bfb2d86abb3130ab1f8a2a797
Jiyeon Park <wldusys34@gmail.com>
2024-07-29 12:17:02 +0900

    Refactor: 준비중 코드 Notice 컴포넌트로 수정

--- src/pages/PreparingPage.tsx
+++ src/pages/PreparingPage.tsx
@@ -1,20 +1,13 @@
-import { Link } from "react-router-dom";
-import prepare from "../assets/images/sad.svg";
+import Notice from "../components/common/Notice";
 
 const PreparingPage = () => {
   return (
     <div className="h-screen flex flex-col items-center justify-center pb-28">
-      <img src={prepare} alt="준비중" />
-      <div className="flex-center flex-col pt-6 gap-4">
-        <span className="text-xl font-semibold">서비스 준비중이에요.</span>
-        <span className="text-gray-500 text-sm">조금만 기다려주세요.</span>
-      </div>
-      <Link
-        to="/"
-        className="min-w-fit bg-black text-white px-7 py-5 rounded-lg mt-14"
-      >
-        홈으로 가기
-      </Link>
+      <Notice
+        isSad={true}
+        title={"서비스 준비중이에요."}
+        text={"조금만 기다려 주세요."}
+      />
     </div>
   );
 };
