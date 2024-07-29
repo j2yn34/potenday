d3587884c6084994a9d913abd72555504300adbe
Jiyeon Park <wldusys34@gmail.com>
2024-07-30 00:55:00 +0900

    Feat: 이미지 클릭시 새창으로 링크 이동하도록 구현

--- src/components/GiftList.tsx
+++ src/components/GiftList.tsx
@@ -36,45 +36,49 @@ const GiftList = ({
   return (
     <div className="flex flex-col gap-3">
       {data.map((gift, index) => (
-        <div
-          className={`flex p-3 h-[132px] rounded-lg bg-white max-w-full ${
-            index === data.length - 1 ? "mb-[100px]" : ""
-          }`}
-          onClick={() => (window.location.href = gift.link)}
+        <a
+          href={gift.link}
           key={gift.id}
-          role="button"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <div className="pr-3">
-            <img
-              className="rounded-lg w-[80px] h-[108px]"
-              src={gift.imgLink}
-              alt="상품 사진"
-            />
-          </div>
-          <div className="flex flex-1 items-start justify-between min-w-0">
-            <div className="flex flex-col flex-1 min-w-0">
-              <p className="font-semibold mb-1 text-ellipsis overflow-hidden line-clamp-2">
-                {gift.name}
-              </p>
-              <div className="text-sm text-gray-800">
-                {gift.price.toLocaleString("ko-KR")}원
-              </div>
+          <div
+            className={`flex p-3 h-[132px] rounded-lg bg-white max-w-full ${
+              index === data.length - 1 ? "mb-[100px]" : ""
+            }`}
+          >
+            <div className="pr-3">
+              <img
+                className="rounded-lg w-[80px] h-[108px]"
+                src={gift.imgLink}
+                alt="상품 사진"
+              />
             </div>
-            <button
-              className="flex-shrink-0 ml-2"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleLike(gift.id);
-              }}
-            >
-              {likedItems[gift.id] ? (
-                <FaHeart size={24} />
-              ) : (
-                <FaRegHeart size={24} />
-              )}
-            </button>
+            <div className="flex flex-1 items-start justify-between min-w-0">
+              <div className="flex flex-col flex-1 min-w-0">
+                <p className="font-semibold mb-1 text-ellipsis overflow-hidden line-clamp-2">
+                  {gift.name}
+                </p>
+                <div className="text-sm text-gray-800">
+                  {gift.price.toLocaleString("ko-KR")}원
+                </div>
+              </div>
+              <button
+                className="flex-shrink-0 ml-2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleLike(gift.id);
+                }}
+              >
+                {likedItems[gift.id] ? (
+                  <FaHeart size={24} />
+                ) : (
+                  <FaRegHeart size={24} />
+                )}
+              </button>
+            </div>
           </div>
-        </div>
+        </a>
       ))}
     </div>
   );
--- src/components/TopHeartCard.tsx
+++ src/components/TopHeartCard.tsx
@@ -2,12 +2,9 @@ import { ProductCard } from "../type";
 
 const TopHeartCard = ({ data }: { data: ProductCard }) => {
   const formattedPrice = data.price.toLocaleString("ko-KR");
-  const goToBuyGift = () => {
-    window.location.href = `${data.link}`;
-  };
 
   return (
-    <button className="card" onClick={goToBuyGift}>
+    <a href={data.link} target="_blank" rel="noopener noreferrer">
       <img className="rounded-lg" src={data.imgLink} alt="상품 사진" />
       <div className="text-start">
         <div className="pt-2 pb-1 text-md font-medium">{data.name}</div>
@@ -15,7 +12,7 @@ const TopHeartCard = ({ data }: { data: ProductCard }) => {
           {formattedPrice}원
         </div>
       </div>
-    </button>
+    </a>
   );
 };
 
