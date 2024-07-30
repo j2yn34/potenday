a5a0cd5342235c43c01588d206e2281b56f4eea1
Jiyeon Park <wldusys34@gmail.com>
2024-07-30 20:57:54 +0900

    Feat: NotFound 페이지 구현

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,17 @@
+import Notice from "../components/common/Notice";
+
 const NotFoundPage = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="h-screen">
+      <Notice
+        isSad={true}
+        title={"페이지를 찾을 수 없어요."}
+        text={"올바른 주소로 다시 검석해 보세요."}
+        nav={"/"}
+        btnName={"홈으로 가기"}
+      />
+    </div>
+  );
 };
 
 export default NotFoundPage;
