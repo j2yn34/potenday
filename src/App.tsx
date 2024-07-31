7441897895791fe1541867157f66cf80f855a0dd
Jiyeon Park <wldusys34@gmail.com>
2024-07-31 17:54:43 +0900

    Feat: 모바일에서 주소창, 툴바 제외 화면 꽉 차게 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
+import useViewportHeight from "./hooks/useViewportHeight";
+import { useEffect } from "react";
 
 const App = () => {
+  const vh = useViewportHeight();
+
+  useEffect(() => {
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, [vh]);
+
   return (
     <BrowserRouter>
-      <Router />
+      <div className="full-height">
+        <Router />
+      </div>
     </BrowserRouter>
   );
 };
