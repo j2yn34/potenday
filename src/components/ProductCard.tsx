4fbe06f7246592720d6a610253b467fd701814c9
Jiyeon Park <wldusys34@gmail.com>
2024-08-02 09:02:47 +0900

    Feat: 홈화면 상품에 하트버튼 추가

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { ProductType } from "../type";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const ProductCard = ({ data }: { data: ProductType }) => {
   const formattedPrice = data.price.toLocaleString("ko-KR");
+  const [liked, setLiked] = useState<boolean>(false);
+
+  const handleHeart = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setLiked(!liked);
+  };
 
   return (
     <a href={data.link} target="_blank" rel="noopener noreferrer">
-      <img className="rounded-lg" src={data.imgLink} alt="상품 사진" />
+      <div className="relative">
+        <img className="rounded-lg" src={data.imgLink} alt="상품 사진" />
+        <button className="absolute bottom-0 right-0" onClick={handleHeart}>
+          {liked ? (
+            <div className="p-3">
+              <FaHeart size={24} className="text-orange-500" />
+            </div>
+          ) : (
+            <div className="p-3">
+              <FaRegHeart size={24} className="text-white" />
+            </div>
+          )}
+        </button>
+      </div>
       <div className="text-start">
         <div className="pt-2 pb-1 text-md font-medium">{data.name}</div>
         <div className="text-sm text-gray-500 font-normal">
