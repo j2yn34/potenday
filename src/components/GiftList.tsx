39fc6268fff9fa2d4f8b2d3a87bacc7ab74942a0
Jiyeon Park <wldusys34@gmail.com>
2024-08-06 07:25:25 +0900

    Refactor: 추천선물리스트 컴포넌트 수정

--- src/components/GiftList.tsx
+++ src/components/GiftList.tsx
@@ -1,22 +1,8 @@
-import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { ProductType } from "../type";
-import { useState } from "react";
 import Notice from "./common/Notice";
+import { ProductListType, ProductType } from "../type";
+import ProductCard from "./ProductCard";
 
-const GiftList = ({ data }: { data: ProductType[] }) => {
-  const [likedItems, setLikedItems] = useState<{ [key: number]: boolean }>({});
-
-  const toggleLike = (id: number) => {
-    setLikedItems((prevLikedItems) => ({
-      ...prevLikedItems,
-      [id]: !prevLikedItems[id],
-    }));
-  };
-
-  if (!Array.isArray(data)) {
-    return null;
-  }
-
+const GiftList = ({ data }: { data: ProductListType[] }) => {
   if (data.length === 0) {
     return (
       <div className="pt-28">
@@ -31,50 +17,17 @@ const GiftList = ({ data }: { data: ProductType[] }) => {
 
   return (
     <div className="flex flex-col gap-3">
-      {data.map((gift, index) => (
-        <a
-          href={gift.link}
-          key={gift.id}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div
-            className={`flex p-3 h-[132px] rounded-lg bg-white max-w-full ${
-              index === data.length - 1 ? "mb-[100px]" : ""
-            }`}
-          >
-            <div className="pr-3">
-              <img
-                className="rounded-lg w-[80px] h-[108px]"
-                src={gift.imgLink}
-                alt="상품 사진"
+      {data.map((gift) => (
+        <div key={gift.category}>
+          <div className="grid grid-cols-2 gap-x-4 gap-y-6 items-start">
+            {gift.products.map((product: ProductType, index: number) => (
+              <ProductCard
+                data={product}
+                key={`${product.productId}+${index}`}
               />
-            </div>
-            <div className="flex flex-1 items-start justify-between min-w-0">
-              <div className="flex flex-col flex-1 min-w-0">
-                <p className="font-semibold mb-1 text-ellipsis overflow-hidden line-clamp-2">
-                  {gift.name}
-                </p>
-                <div className="text-sm text-gray-800">
-                  {gift.price.toLocaleString("ko-KR")}원
-                </div>
-              </div>
-              <button
-                className="flex-shrink-0 ml-2"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  toggleLike(gift.id);
-                }}
-              >
-                {likedItems[gift.id] ? (
-                  <FaHeart size={24} />
-                ) : (
-                  <FaRegHeart size={24} />
-                )}
-              </button>
-            </div>
+            ))}
           </div>
-        </a>
+        </div>
       ))}
     </div>
   );
