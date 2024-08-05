24d72a2cd306a08f469c0740e1576c4ddbdc051d
Jiyeon Park <wldusys34@gmail.com>
2024-08-06 06:52:13 +0900

    Feat: 홈화면 배경이미지 설정 및 디자인 수정

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,29 +3,42 @@ import Header from "../components/common/Header";
 import Lottie from "lottie-react";
 import home from "../assets/lottie/home.json";
 import ProductSection from "../components/ProductSection";
+import icon from "../assets/images/tabIcon.png";
 
 const Index = () => {
   return (
-    <>
-      <Header />
-      <div className="px-5">
-        <div className="flex flex-col items-center mb-9">
-          <h1 className="text-center text-2xl font-semibold leading-9 mt-10 mb-6">
-            1분 만에 고르는
-            <br />
-            당신에게 딱 맞는 선물
-          </h1>
-          <Link
-            to="/voice"
-            className="flex-center w-[158px] h-14 bg-black text-white rounded-lg"
-          >
-            선물 고르러 가기
-          </Link>
-          <Lottie className="w-[240px] h-[240px] mt-3" animationData={home} />
+    <div className="bg-white">
+      <div className="home-bg">
+        <Header />
+        <div className="px-5">
+          <div className="flex flex-col items-center mb-9">
+            <h1 className="text-center text-2xl font-semibold leading-9 mt-10 mb-6">
+              1분 만에 고르는
+              <br />
+              당신에게 딱 맞는 선물
+            </h1>
+            <Link
+              to="/voice"
+              className="flex-center w-[158px] h-14 bg-black text-white rounded-lg"
+            >
+              선물 고르러 가기
+            </Link>
+            <Lottie className="w-[240px] h-[240px] mt-3" animationData={home} />
+          </div>
+
+          <div className="flex-center flex-col py-12">
+            <img src={icon} className="w-[32px] mb-[10px]" />
+            <h1 className="text-[22px] font-semibold leading-9 mb-2">
+              T!FY의 인기선물
+            </h1>
+            <span className="text-gray-500 text-sm">
+              많은 사람들이 찜한 인기 선물을 구경해보세요!
+            </span>
+          </div>
+          <ProductSection />
         </div>
-        <ProductSection />
       </div>
-    </>
+    </div>
   );
 };
 
