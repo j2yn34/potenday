f3b612aeb1c25943bae4fe042c8cd85410d5124d
Jiyeon Park <wldusys34@gmail.com>
2024-08-06 22:05:59 +0900

    Style: 오타 수정

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
       <Notice
         isSad={true}
         title={"페이지를 찾을 수 없어요."}
-        text={"올바른 주소로 다시 검석해 보세요."}
+        text={"올바른 주소로 다시 검색해 보세요."}
         nav={"/"}
         btnName={"홈으로 가기"}
       />
