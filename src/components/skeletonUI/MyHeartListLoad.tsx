ec15a0085ec95b3fe83805b6cab30481ed96904e
Jiyeon Park <wldusys34@gmail.com>
2024-08-08 18:12:42 +0900

    Design: 스켈레톤ui 수정

--- src/components/skeletonUI/MyHeartListLoad.tsx
+++ src/components/skeletonUI/MyHeartListLoad.tsx
@@ -1,20 +1,20 @@
 const MyHeartListLoad = () => {
   return (
-    <div className="relative w-full full-height overflow-hidden px-5 mx-auto max-w-screen-lg bg-purple-50">
-      <div className="pb-3 grid grid-cols-5 gap-x-4 animate-pulse">
-        <div className="w-full h-5 bg-gray-300"></div>
-        <div className="w-full h-5 bg-gray-300"></div>
-        <div className="w-full h-5 bg-gray-300"></div>
-        <div className="w-full h-5 bg-gray-300"></div>
+    <div className="relative w-full full-height overflow-hidden mx-auto max-w-screen-lg bg-purple-50">
+      <div className="pb-3 flex gap-5 animate-pulse">
+        <div className="w-[40px] h-5 bg-gray-300 rounded-sm"></div>
+        <div className="w-[50px] h-5 bg-gray-300 rounded-sm"></div>
+        <div className="w-[50px] h-5 bg-gray-300 rounded-sm"></div>
+        <div className="w-[50px] h-5 bg-gray-300 rounded-sm"></div>
       </div>
       <div className="w-screen -ml-5 h-[6px] bg-[#E7E5F2] my-2" />
       <div className="flex justify-end">
-        <div className="w-[76px] h-[32px] rounded-full mt-2 bg-gray-300"></div>
+        <div className="w-[76px] h-[32px] rounded-full mt-2 bg-gray-300" />
       </div>
       {Array.from(Array(2)).map((_, index) => (
         <div className="pb-14 animate-pulse" key={index}>
           <div className="w-[80px] h-[24px] rounded bg-gray-300 mt-3 mb-4 text-xl font-semibold"></div>
-          <div className="grid grid-cols-2 gap-x-4 gap-y-12 items-start">
+          <div className="grid grid-cols-2 gap-x-4 gap-y-14 items-start">
             {Array.from(Array(4)).map((_, index) => (
               <div className="w-full h-48 card" key={index}>
                 <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-300" />
--- src/components/skeletonUI/ProductListLoad.tsx
+++ src/components/skeletonUI/ProductListLoad.tsx
@@ -4,16 +4,17 @@ const ProductListLoad = () => {
       <div>
         {Array.from(Array(3)).map((_, index) => (
           <div className="pb-14 animate-pulse" key={index}>
-            <div className="w-[70px] h-[24px] rounded bg-gray-300 mt-3 mb-4 text-xl font-semibold"></div>
-            <div className="grid grid-cols-2 gap-x-4 gap-y-8 items-start">
+            <div className="w-[70px] h-[24px] rounded bg-gray-300 mt-3 mb-4"></div>
+            <div className="grid grid-cols-2 gap-x-4 gap-y-14 items-start">
               {Array.from(Array(4)).map((_, index) => (
-                <button className="w-full h-48 card" key={index}>
-                  <div className="w-full h-36 rounded-lg bg-gray-300" />
+                <div className="w-full h-48 card" key={index}>
+                  <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-300" />
                   <div className="w-full h-12 mt-2 text-start">
                     <div className="w-full h-4 mt-2 bg-gray-300"></div>
                     <div className="w-full h-4 mt-2 bg-gray-300"></div>
+                    <div className="w-[30%] h-3 mt-2 bg-gray-300"></div>
                   </div>
-                </button>
+                </div>
               ))}
             </div>
           </div>
