9c1383e27a14ca574921556a8bdd0b34264ff681
Jiyeon Park <wldusys34@gmail.com>
2024-10-08 01:35:32 +0900

    Fix: 관심목록 페이지 조건부 렌더링의 기준을 token으로 수정
    - showModal 상태 제거, token이 없으면 모달이 렌더링되도록 구현

--- src/pages/MyHeartListPage.tsx
+++ src/pages/MyHeartListPage.tsx
@@ -2,57 +2,40 @@ import { useRecoilValue } from "recoil";
 import MyHeartList from "../components/MyHeartList";
 import { accessTokenState } from "../state/recoil";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import ConfirmModal from "../components/common/ConfirmModal";
 import ScrollToTopButton from "../components/buttons/ScrollToTopBtn";
 
 const MyHeartListPage = () => {
   const token = useRecoilValue<string>(accessTokenState);
   const navigate = useNavigate();
-  const [showModal, setShowModal] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (!token) {
-      openModal();
-    } else {
-      setShowModal(false);
-    }
-  }, []);
-
-  const openModal = () => {
-    setShowModal(true);
-    document.body.style.overflow = "hidden";
-  };
-
-  const cancle = () => {
-    setShowModal(false);
+  const cancel = () => {
     navigate("/");
     document.body.style.overflow = "auto";
   };
 
   const goHome = () => {
-    setShowModal(false);
     navigate("/login");
     document.body.style.overflow = "auto";
   };
 
   return (
     <div>
-      {showModal ? (
-        <ConfirmModal
-          isSad={false}
-          title={"로그인이 필요한 기능이에요"}
-          text={"관심 선물을 저장하고 공유해 보세요"}
-          leftBtn={cancle}
-          confirm={goHome}
-          leftName={"취소"}
-          rightName={"로그인 하기"}
-        />
-      ) : (
+      {token ? (
         <>
           <MyHeartList />
           <ScrollToTopButton />
         </>
+      ) : (
+        <ConfirmModal
+          isSad={false}
+          title={"로그인이 필요한 기능이에요"}
+          text={"관심 선물을 저장하고 공유해 보세요"}
+          leftBtn={cancel}
+          confirm={goHome}
+          leftName={"취소"}
+          rightName={"로그인 하기"}
+        />
       )}
     </div>
   );
